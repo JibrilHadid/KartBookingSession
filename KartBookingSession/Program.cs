37f20918a6d0498705c2abebdc1fa267b03c3341
ac146662@avcol.school.nz <ac146662@avcol.school.nz>
2025-05-16 09:59:32 +1200

    I tested the connection

--- KartBookingSession/Program.cs
+++ KartBookingSession/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace KartBookingSession
+﻿using KartBookingSession.Repositories;
+
+namespace KartBookingSession
 {
-    internal class Program
+    public class Program
     {
+        private static StorageManager storageManager;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=KartBookingSession;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+
+            storageManager = new StorageManager
+                (connectionString);
         }
     }
 }
