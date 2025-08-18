d03c29600b9333c17155ed1a1e8617cad4e9237e
Jibril Hadid <ac146662@avcol.school.nz>
2025-08-18 13:21:41 +1200

    Updated my mdf file path

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -695,7 +695,3 @@ VALUES
 ('68', 'coach68@email.com', '8901234573', 'Intermediate'),
 ('69', 'coach69@email.com', '9012345684', 'Advanced'),
 ('70', 'coach70@email.com', '0123456795', 'Beginner');
-
-
-
-
